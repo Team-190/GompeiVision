2356c94877ba3e4c97968489480402b8e3e0b99d
ElliotScher <ecscher84@gmail.com>
2025-06-30 20:14:06 -0400

    docker builds with wpilib built from source

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,23 +1,10 @@
 #include <apriltag/apriltag.h>
-#include <apriltag/tag36h11.h>
+#include <ntcore/networktables/NetworkTableInstance.h>
 
-#include <iostream>
 #include <opencv2/opencv.hpp>
-
 int main() {
-  // Test OpenCV: create a blank image and print its size
-  const cv::Mat image = cv::Mat::zeros(cv::Size(320, 240), CV_8UC3);
-  std::cout << "OpenCV image created. Size: " << image.cols << "x" << image.rows
-            << std::endl;
-
-  apriltag_family_t* tf = tag36h11_create();
-  apriltag_detector_t* td = apriltag_detector_create();
-  apriltag_detector_add_family(td, tf);
-  std::cout << "AprilTag detector created with tag36h11." << std::endl;
-
-  // Cleanup
-  apriltag_detector_destroy(td);
-  tag36h11_destroy(tf);
-
+  nt::NetworkTableInstance ntinst = nt::NetworkTableInstance::GetDefault();
+  apriltag_detection_t* detection = nullptr;
+  cv::Mat image = cv::Mat();
   return 0;
 }
