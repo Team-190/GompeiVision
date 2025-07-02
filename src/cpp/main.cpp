a34c5456ce4061d3704c106059abdf130a968812
ElliotScher <ecscher84@gmail.com>
2025-07-02 15:46:52 -0400

    add arm build

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -5,7 +5,7 @@
 int main() {
   nt::NetworkTableInstance ntinst = nt::NetworkTableInstance::GetDefault();
   apriltag_detection_t* detection = nullptr;
-  cv::Mat image = cv::Mat();
+  auto image = cv::Mat();
   std::cout << "Hello Gompei!" << std::endl;
   return 0;
 }
