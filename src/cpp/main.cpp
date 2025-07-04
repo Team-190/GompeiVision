2b0345e3ac635cd1e5bfe8c9a55deac631d4d006
ElliotScher <ecscher84@gmail.com>
2025-07-04 01:34:09 -0400

    add openpnp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,11 +1,13 @@
 #include <apriltag/apriltag.h>
 #include <ntcore/networktables/NetworkTableInstance.h>
+#include <openpnp-capture.h>
 
 #include <opencv2/opencv.hpp>
 int main() {
   nt::NetworkTableInstance ntinst = nt::NetworkTableInstance::GetDefault();
   apriltag_detection_t* detection = nullptr;
   auto image = cv::Mat();
+  CapResult result = CapResult();
   std::cout << "Hello Gompei!" << std::endl;
   return 0;
 }
