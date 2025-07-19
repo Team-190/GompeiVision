a9a62f3d114f853ee6ee62d2be7b1a02cd7580b1
ElliotScher <ecscher84@gmail.com>
2025-07-18 20:03:25 -0400

    fix?

--- src/cpp/estimator/CameraPoseEstimator.cpp
+++ src/cpp/estimator/CameraPoseEstimator.cpp
@@ -3,6 +3,7 @@
 #include <frc/geometry/Transform3d.h>
 
 #include <opencv2/calib3d.hpp>
+#include <ranges>
 #include <vector>
 
 #include "util/PoseUtils.h"
@@ -11,158 +12,168 @@ void CameraPoseEstimator::estimatePose(
     const FiducialImageObservation& observation, AprilTagResult& result,
     const cv::Mat& cameraMatrix, const cv::Mat& distCoeffs, double tag_size_m,
     const std::map<int, frc::Pose3d>& field_layout) {
+  if (field_layout.empty()) {
+    return;
+  }
+  std::vector<int> tags_used_ids;
+  std::vector<cv::Point2f> all_image_points;
+  std::vector<cv::Point3f> all_object_points;
+  std::vector<frc::Pose3d> tagPoses;
+
   const std::vector<cv::Point3d> standard_object_points = {
       {-tag_size_m / 2.0f, tag_size_m / 2.0f, 0.0f},  // Top-left
       {tag_size_m / 2.0f, tag_size_m / 2.0f, 0.0f},   // Top-right
       {tag_size_m / 2.0f, -tag_size_m / 2.0f, 0.0f},  // Bottom-right
       {-tag_size_m / 2.0f, -tag_size_m / 2.0f, 0.0f}  // Bottom-left
   };
-  if (!field_layout.empty()) {
-    std::vector<int> tags_used_ids;
-    std::vector<cv::Point2f> all_image_points;
-    std::vector<cv::Point3f> all_object_points;
-    // Define the corner locations in the tag's local frame (X out, Y left, Z
-    // up) Order must match the detector output: TL, TR, BR, BL
-    const auto corner_tl =
-        frc::Transform3d({0_m, units::meter_t{tag_size_m / 2.0},
-                          units::meter_t{tag_size_m / 2.0}},
-                         {});
-    const auto corner_tr =
-        frc::Transform3d({0_m, -units::meter_t{tag_size_m / 2.0},
-                          units::meter_t{tag_size_m / 2.0}},
-                         {});
-    const auto corner_br =
-        frc::Transform3d({0_m, -units::meter_t{tag_size_m / 2.0},
-                          -units::meter_t{tag_size_m / 2.0}},
-                         {});
-    const auto corner_bl =
-        frc::Transform3d({0_m, units::meter_t{tag_size_m / 2.0},
-                          -units::meter_t{tag_size_m / 2.0}},
-                         {});
-    const std::vector corner_transforms = {corner_tl, corner_tr, corner_br,
-                                           corner_bl};
 
-    for (size_t i = 0; i < observation.tag_ids.size(); ++i) {
-      int tag_id = observation.tag_ids[i];
-
-      if (auto it = field_layout.find(tag_id); it != field_layout.end()) {
-        tags_used_ids.push_back(tag_id);
-        const frc::Pose3d& tag_field_pose = it->second;
-
-        // Calculate the 3D coordinates of each corner in the field frame
-        for (const auto& transform : corner_transforms) {
-          frc::Pose3d corner_field_pose = tag_field_pose.TransformBy(transform);
-          all_object_points.push_back(PoseUtils::wpilibTranslationToOpenCV(
-              corner_field_pose.Translation()));
-        }
-
-        // Add the corresponding 2D image points
-        for (size_t j = 0; j < observation.corners_pixels[i].size(); j += 2) {
-          all_image_points.emplace_back(observation.corners_pixels[i][j],
-                                        observation.corners_pixels[i][j + 1]);
-        }
-      }
+  for (int i = 0; i < observation.tag_ids.size(); i++) {
+    auto tagPose = frc::Pose3d();
+    for (const auto& key : field_layout | std::views::keys) {
+      if (key == observation.tag_ids[i]) tagPose = field_layout.at(key);
     }
 
-    if (tags_used_ids.size() == 1) {
-      // Use IPPE to get the two ambiguous poses for the tag
-      std::vector<cv::Mat> rvecs, tvecs;
-      std::vector<double> errors;
+    if (PoseUtils::isPoseZero(tagPose)) {
+      auto corner_0 =
+          tagPose + frc::Transform3d(frc::Translation3d(
+                                         0_m, units::meter_t(tag_size_m / 2.0),
+                                         units::meter_t(-tag_size_m / 2.0)),
+                                     frc::Rotation3d());
+      auto corner_1 =
+          tagPose + frc::Transform3d(frc::Translation3d(
+                                         0_m, units::meter_t(-tag_size_m / 2.0),
+                                         units::meter_t(-tag_size_m / 2.0)),
+                                     frc::Rotation3d());
+      auto corner_2 =
+          tagPose + frc::Transform3d(frc::Translation3d(
+                                         0_m, units::meter_t(-tag_size_m / 2.0),
+                                         units::meter_t(tag_size_m / 2.0)),
+                                     frc::Rotation3d());
+      auto corner_3 =
+          tagPose + frc::Transform3d(frc::Translation3d(
+                                         0_m, units::meter_t(tag_size_m / 2.0),
+                                         units::meter_t(tag_size_m / 2.0)),
+                                     frc::Rotation3d());
 
-      std::cout << "Defining vectors and Mats" << std::endl;
+      all_object_points.push_back(
+          PoseUtils::wpilibTranslationToOpenCV(corner_0.Translation()));
+      all_object_points.push_back(
+          PoseUtils::wpilibTranslationToOpenCV(corner_1.Translation()));
+      all_object_points.push_back(
+          PoseUtils::wpilibTranslationToOpenCV(corner_2.Translation()));
+      all_object_points.push_back(
+          PoseUtils::wpilibTranslationToOpenCV(corner_3.Translation()));
 
-      cv::solvePnPGeneric(all_object_points, all_image_points, cameraMatrix,
-                          distCoeffs, rvecs, tvecs, false,
-                          cv::SOLVEPNP_IPPE_SQUARE, cv::noArray(),
-                          cv::noArray(), errors);
+      all_image_points.push_back(cv::Point2f(observation.corners_pixels[i][0],
+                                             observation.corners_pixels[i][1]));
+      all_image_points.push_back(cv::Point2f(observation.corners_pixels[i][2],
+                                             observation.corners_pixels[i][3]));
+      all_image_points.push_back(cv::Point2f(observation.corners_pixels[i][4],
+                                             observation.corners_pixels[i][5]));
+      all_image_points.push_back(cv::Point2f(observation.corners_pixels[i][6],
+                                             observation.corners_pixels[i][7]));
 
-      std::cout << "Solved PNP lol" << std::endl;
-
-      // A valid solution must have two poses and corresponding errors.
-      if (rvecs.size() == 2 && tvecs.size() == 2 && errors.size() == 2) {
-        std::cout << "Valid Capture" << std::endl;
-        CameraPoseObservation pose;
-        pose.tag_ids = observation.tag_ids;
-        pose.error_0 = errors[0];
-        pose.error_1 = errors[1];
-
-        std::cout << "defined pose" << std::endl;
-
-        auto camera_to_tag_pose_0 =
-            PoseUtils::openCvPoseToWpilib(rvecs[0], tvecs[0]);
-        auto camera_to_tag_pose_1 =
-            PoseUtils::openCvPoseToWpilib(rvecs[1], tvecs[1]);
-
-        frc::Pose3d field_to_tag_pose =
-            field_layout.find(observation.tag_ids[0])->second;
-
-        auto camera_to_tag_0 =
-            frc::Transform3d(camera_to_tag_pose_0.Translation(),
-                             camera_to_tag_pose_0.Rotation());
-        auto camera_to_tag_1 =
-            frc::Transform3d(camera_to_tag_pose_1.Translation(),
-                             camera_to_tag_pose_1.Rotation());
-
-        auto field_to_camera_0 =
-            field_to_tag_pose.TransformBy(camera_to_tag_0.Inverse());
-        auto field_to_camera_1 =
-            field_to_tag_pose.TransformBy(camera_to_tag_1.Inverse());
-
-        auto field_to_camera_pose_0 = frc::Pose3d(
-            field_to_camera_0.Translation(), field_to_camera_0.Rotation());
-        auto field_to_camera_pose_1 = frc::Pose3d(
-            field_to_camera_1.Translation(), field_to_camera_1.Rotation());
-
-        std::cout << "Transforms" << std::endl;
-
-        pose.pose_0 = field_to_camera_pose_0;
-        pose.pose_1 = field_to_camera_pose_1;
-
-        std::cout << "Pose: " << pose.pose_0.X().value() << ", "
-                  << pose.pose_0.Y().value() << std::endl;
-
-        result.multi_tag_pose = pose;
-      }
-    } else if (tags_used_ids.size() > 1) {
-      std::cout << "> 1 tag found" << std::endl;
-
-      std::vector<cv::Mat> multi_rvec, multi_tvec;
-      std::vector<double> errors;
-
-      std::cout << "Defining vectors and Mats" << std::endl;
-
-      // Use a robust solver like SQPNP for a single, stable pose.
-      cv::solvePnPGeneric(all_object_points, all_image_points, cameraMatrix,
-                          distCoeffs, multi_rvec, multi_tvec, false,
-                          cv::SOLVEPNP_SQPNP, cv::noArray(), cv::noArray(),
-                          errors);
-
-      std::cout << "Solved PNP lol" << std::endl;
-
-      // The result of solvePnP is the pose of the field origin in the
-      // camera's frame.
-      frc::Pose3d camera_to_field_pose =
-          PoseUtils::openCvPoseToWpilib(multi_rvec[0], multi_tvec[0]);
-
-      std::cout << "transform cv to wpilib pose" << std::endl;
-
-      // We need the inverse: the pose of the camera in the field's frame.
-      auto camera_to_field = frc::Transform3d(
-          camera_to_field_pose.Translation(), camera_to_field_pose.Rotation());
-      auto field_to_camera = camera_to_field.Inverse();
-      auto field_to_camera_pose = frc::Pose3d(field_to_camera.Translation(),
-                                              field_to_camera.Rotation());
-
-      std::cout << "Invert transform" << std::endl;
-
-      CameraPoseObservation cam_pose;
-      cam_pose.pose_0 = field_to_camera_pose;
-      cam_pose.error_0 = errors[0];
-      cam_pose.tag_ids = tags_used_ids;
-
-      result.multi_tag_pose = cam_pose;
-
-      std::cout << "assigned" << std::endl;
+      tags_used_ids.push_back(observation.tag_ids[i]);
+      tagPoses.push_back(tagPose);
     }
+  }
+
+  if (tags_used_ids.size() == 1) {
+    // Use IPPE to get the two ambiguous poses for the tag
+    std::vector<cv::Mat> rvecs, tvecs;
+    std::vector<double> errors;
+
+    std::cout << "Defining vectors and Mats" << std::endl;
+
+    cv::solvePnPGeneric(standard_object_points, all_image_points, cameraMatrix,
+                        distCoeffs, rvecs, tvecs, false,
+                        cv::SOLVEPNP_IPPE_SQUARE, cv::noArray(), cv::noArray(),
+                        errors);
+
+    std::cout << "Solved PNP lol" << std::endl;
+
+    // A valid solution must have two poses and corresponding errors.
+    if (rvecs.size() == 2 && tvecs.size() == 2 && errors.size() == 2) {
+      std::cout << "Valid Capture" << std::endl;
+      CameraPoseObservation pose;
+      pose.tag_ids = observation.tag_ids;
+      pose.error_0 = errors[0];
+      pose.error_1 = errors[1];
+
+      std::cout << "defined pose" << std::endl;
+
+      auto camera_to_tag_pose_0 =
+          PoseUtils::openCvPoseToWpilib(rvecs[0], tvecs[0]);
+      auto camera_to_tag_pose_1 =
+          PoseUtils::openCvPoseToWpilib(rvecs[1], tvecs[1]);
+
+      frc::Pose3d field_to_tag_pose =
+          field_layout.find(observation.tag_ids[0])->second;
+
+      auto camera_to_tag_0 = frc::Transform3d(
+          camera_to_tag_pose_0.Translation(), camera_to_tag_pose_0.Rotation());
+      auto camera_to_tag_1 = frc::Transform3d(
+          camera_to_tag_pose_1.Translation(), camera_to_tag_pose_1.Rotation());
+
+      auto field_to_camera_0 =
+          field_to_tag_pose.TransformBy(camera_to_tag_0.Inverse());
+      auto field_to_camera_1 =
+          field_to_tag_pose.TransformBy(camera_to_tag_1.Inverse());
+
+      auto field_to_camera_pose_0 = frc::Pose3d(field_to_camera_0.Translation(),
+                                                field_to_camera_0.Rotation());
+      auto field_to_camera_pose_1 = frc::Pose3d(field_to_camera_1.Translation(),
+                                                field_to_camera_1.Rotation());
+
+      std::cout << "Transforms" << std::endl;
+
+      pose.pose_0 = field_to_camera_pose_0;
+      pose.pose_1 = field_to_camera_pose_1;
+
+      std::cout << "Pose: " << pose.pose_0.X().value() << ", "
+                << pose.pose_0.Y().value() << std::endl;
+
+      result.multi_tag_pose = pose;
+    }
+  } else if (tags_used_ids.size() > 1) {
+    std::cout << "> 1 tag found" << std::endl;
+
+    std::vector<cv::Mat> multi_rvec, multi_tvec;
+    std::vector<double> errors;
+
+    std::cout << "Defining vectors and Mats" << std::endl;
+
+    // Use a robust solver like SQPNP for a single, stable pose.
+    cv::solvePnPGeneric(all_object_points, all_image_points, cameraMatrix,
+                        distCoeffs, multi_rvec, multi_tvec, false,
+                        cv::SOLVEPNP_SQPNP, cv::noArray(), cv::noArray(),
+                        errors);
+
+    std::cout << "Solved PNP lol" << std::endl;
+
+    // The result of solvePnP is the pose of the field origin in the
+    // camera's frame.
+    frc::Pose3d camera_to_field_pose =
+        PoseUtils::openCvPoseToWpilib(multi_rvec[0], multi_tvec[0]);
+
+    std::cout << "transform cv to wpilib pose" << std::endl;
+
+    // We need the inverse: the pose of the camera in the field's frame.
+    auto camera_to_field = frc::Transform3d(camera_to_field_pose.Translation(),
+                                            camera_to_field_pose.Rotation());
+    auto field_to_camera = camera_to_field.Inverse();
+    auto field_to_camera_pose =
+        frc::Pose3d(field_to_camera.Translation(), field_to_camera.Rotation());
+
+    std::cout << "Invert transform" << std::endl;
+
+    CameraPoseObservation cam_pose;
+    cam_pose.pose_0 = field_to_camera_pose;
+    cam_pose.error_0 = errors[0];
+    cam_pose.tag_ids = tags_used_ids;
+
+    result.multi_tag_pose = cam_pose;
+
+    std::cout << "assigned" << std::endl;
   }
 }
