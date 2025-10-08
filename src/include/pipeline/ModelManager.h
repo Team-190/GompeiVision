2668272caf665d96b9c618cd27d3d1c741a6625c
Anshu Adiga <aadiga@wpi.edu>
2025-10-08 17:02:44 -0400

    add YOLOv11 model

--- src/include/pipeline/ModelManager.h
+++ src/include/pipeline/ModelManager.h
@@ -12,7 +12,7 @@ struct ObjectModel {
 // Define the global, constant list of all available models.
 inline const std::vector<ObjectModel> available_models = {
     // Index 0: Your primary model for reefscape objects
-    {"/usr/share/GompeiVision/reefscape_yolov8n.onnx",
+    {"/usr/share/GompeiVision/reefscape_yolo11n.onnx",
      "/usr/share/GompeiVision/reefscape.names"},
     // To add a new model, just add a new line here.
 };
