e59e993b5673d3df4058ffd30597cef2e5cb3a8a
Anshu Adiga <aadiga@wpi.edu>
2025-10-13 19:59:17 -0400

    fix ppp (5th try)

--- src/cpp/detector/ObjectDetector.cpp
+++ src/cpp/detector/ObjectDetector.cpp
@@ -35,7 +35,7 @@ ObjectDetector::ObjectDetector(const std::string& model_path,
         .tensor()
         .set_element_type(ov::element::u8)  // Input will be uint8 (0-255)
         .set_shape({1, 640, 640, 3})
-        .set_layout("NHWC")  // OpenCV format: batch, height, width, channels
+        .set_layout("NHWC");  // OpenCV format: batch, height, width, channels
 
     ppp.input().model().set_layout("NCHW");
     ppp.input()
